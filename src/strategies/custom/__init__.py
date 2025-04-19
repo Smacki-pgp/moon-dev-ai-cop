e44904eadfb572a89648a6ba91f2ea901fba723d
Smacki-pgp <jocysmodestas1@gmail.com>
2025-04-19 16:06:18 +0300

    Fixing

--- src/strategies/custom/__init__.py
+++ src/strategies/custom/__init__.py
@@ -3,6 +3,6 @@
 """
 from src.strategies.base_strategy import BaseStrategy
 from .example_strategy import ExampleStrategy
-from .private_my_strategy import MyStrategy
+from .private_flux_vwap import FluxVWAPStrategy
 
 __all__ = ['ExampleStrategy', 'MyStrategy'] 
